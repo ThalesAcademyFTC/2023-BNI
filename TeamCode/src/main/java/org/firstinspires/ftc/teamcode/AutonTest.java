a4096986601b35be335a5839809218bfe8f9ded9
LakelanJonesThales <lakelan.jones@students.thalesacademy.org>
2024-01-08 16:35:23 -0500

    Added a short auton test program

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonTest.java
@@ -0,0 +1,55 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@Autonomous(name="AutonTest")
+public class AutonTest extends LinearOpMode {
+
+    private Spark robot;
+
+    private ElapsedTime runtime = new ElapsedTime();
+
+    private Tagger tagger;
+
+    private Spark.Team team = Spark.Team.BLUE;
+
+
+
+
+    @Override
+    public void runOpMode() {
+        robot = new Spark(this, Spark.Drivetrain.MECHANUM);
+
+        // TODO Change the team to whichever team this auton is for
+        tagger = new Tagger( this, robot.webcamName, team);
+        runtime.reset();
+
+        tagger.init();
+
+        waitForStart();
+
+        robot.moveForwardInches(12, 0.5);
+
+        
+        // This just fills telemetry data with the detected tag.
+        if (opModeIsActive()) {
+            while (opModeIsActive()) {
+
+                tagger.scanWithTelemetry();
+
+                // Push telemetry to the Driver Station.
+                telemetry.update();
+
+                // Share the CPU.
+                sleep(20);
+            }
+        }
+
+        // Save more CPU resources when camera is no longer needed.
+        tagger.close();
+
+    }
+
+}
