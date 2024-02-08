6cc24c9ea6f61b4b85437d7b2ba9ea760baea670
Gabe Walker <gjwalke2@ncsu.edu>
2024-02-07 20:31:35 -0500

    Fixed my demo autons, someone removed the claw function. Also, you guys have a typo in RedPlacePurple, but im not fixing it LOL.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/demo/ButtonPress.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/demo/ButtonPress.java
@@ -49,7 +49,7 @@ public class ButtonPress extends OpMode {
         //Basically, if the button was pressed in this iteration but not the previous one.
         // Only runs once, even if button is held down.
         if (currentGamepad1.a && !previousGamepad1.a) {
-            robot.setClawServo( robot.clawServo.getPosition() + 0.05 );
+           // robot.setClawServo( robot.clawServo.getPosition() + 0.05 );
         }
 
 
