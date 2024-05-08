52332d16f6108f911852bca69448a494e23ca411
Loaff! <alexander.larson-dick27@thalesacademy.org>
2024-05-08 17:13:59 -0400

    Re coded lights

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Lights.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Lights.java
@@ -9,23 +9,56 @@ public class Lights extends OpMode {
 
     Spark robot;
 
-    RevBlinkinLedDriver lights;
 
     public void init() {
-
+        robot = new Spark(this, Spark.Drivetrain.MECHANUM );
     }
 
     @Override
     public void loop() {
 
+        if (gamepad1.a) {
+            robot.lights.setPattern(RevBlinkinLedDriver.BlinkinPattern.RAINBOW_OCEAN_PALETTE);
+        }
+        if (gamepad1.b) {
+            robot.lights.setPattern(RevBlinkinLedDriver.BlinkinPattern.RAINBOW_LAVA_PALETTE);
+        }
+        if(gamepad1.x) {
+            robot.lights.setPattern(RevBlinkinLedDriver.BlinkinPattern.RAINBOW_FOREST_PALETTE);
+        }
         if (gamepad1.y) {
-            robot.lights.setPattern(RevBlinkinLedDriver.BlinkinPattern.TWINKLES_PARTY_PALETTE);
-
-        if(gamepad1.right_trigger > 0.5){
-            robot.lights.setPattern(RevBlinkinLedDriver.BlinkinPattern.TWINKLES_LAVA_PALETTE);
-        }else
-            robot.lights.setPattern(RevBlinkinLedDriver.BlinkinPattern.TWINKLES_FOREST_PALETTE);
+            robot.lights.setPattern(RevBlinkinLedDriver.BlinkinPattern.BEATS_PER_MINUTE_RAINBOW_PALETTE);
+        }
+        if (gamepad1.dpad_down) {
+            robot.lights.setPattern(RevBlinkinLedDriver.BlinkinPattern.COLOR_WAVES_RAINBOW_PALETTE);
+        }
+        if(gamepad1.dpad_up) {
+            robot.lights.setPattern(RevBlinkinLedDriver.BlinkinPattern.SINELON_RAINBOW_PALETTE);
+        }
+        if (gamepad1.dpad_left) {
+            robot.lights.setPattern(RevBlinkinLedDriver.BlinkinPattern.TWINKLES_RAINBOW_PALETTE);
+        }
+        if (gamepad1.dpad_right) {
+            robot.lights.setPattern(RevBlinkinLedDriver.BlinkinPattern.RAINBOW_PARTY_PALETTE);
+        }
+        if(gamepad1.left_bumper) {
+            robot.lights.setPattern(RevBlinkinLedDriver.BlinkinPattern.RAINBOW_RAINBOW_PALETTE);
+        }
+        if (gamepad1.right_bumper) {
+            robot.lights.setPattern(RevBlinkinLedDriver.BlinkinPattern.RAINBOW_WITH_GLITTER);
+        }
+        if (gamepad1.left_trigger > 0.25) {
+            robot.lights.setPattern(RevBlinkinLedDriver.BlinkinPattern.STROBE_WHITE);
+        }
+        if(gamepad1.right_trigger > 0.25) {
+            robot.lights.setPattern(RevBlinkinLedDriver.BlinkinPattern.CP1_STROBE);
+        }
+        if (gamepad1.left_stick_button) {
+            robot.lights.setPattern(RevBlinkinLedDriver.BlinkinPattern.CP2_STROBE);
+        }
+        if (gamepad1.right_stick_button) {
+            robot.lights.setPattern(RevBlinkinLedDriver.BlinkinPattern.CP1_HEARTBEAT_FAST);
         }
     }
-
-}                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                //For Billy Dignam. Bless his soul. :)
+}
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 //For Billy Dignam. Bless his soul. :)
